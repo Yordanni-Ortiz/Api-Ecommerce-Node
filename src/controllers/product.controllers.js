90889afd02b65abcb7d75bb9dca9f8e7a28a8711
Yordanni Ortiz <yordannimod@gmail.com>
2023-05-03 18:13:30 -0400

    relation cloudinary_img

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -1,10 +1,11 @@
 const catchError = require('../utils/catchError');
 const Product = require('../models/Product');
-const ProductImg = require('../models/ProductImg');
+/*const ProductImg = require('../models/ProductImg');*/
+const CloudinaryImg = require('../models/CloudinaryImg');
 const Category = require('../models/Category');
 
 const getAll = catchError(async(req, res) => {
-    const results = await Product.findAll({ include: [ ProductImg, Category ] });
+    const results = await Product.findAll({ include: [ CloudinaryImg, Category ] });
     return res.json(results);
 });
 
@@ -15,7 +16,7 @@ const create = catchError(async(req, res) => {
 
 const getOne = catchError(async(req, res) => {
     const { id } = req.params;
-    const result = await Product.findByPk(id, { include: [ ProductImg, Category ] });
+    const result = await Product.findByPk(id, { include: [ CloudinaryImg, Category ] });
     if(!result) return res.sendStatus(404);
     return res.json(result);
 });
